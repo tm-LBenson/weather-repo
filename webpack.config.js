ac7fa0ad727c54b0bc297a63ba19f45639534cae
Lewis Benson <tm.lewisbenson@gmail.com>
2025-05-02 18:34:12 -0400

    add webpack to config

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ import path from "node:path";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import ReactRefreshWebpackPlugin from "@pmmmwh/react-refresh-webpack-plugin";
 import DotenvWebpackPlugin from "dotenv-webpack";
+import webpack from "webpack";
 
 export default (_env, argv) => {
   const prod = argv.mode === "production";
