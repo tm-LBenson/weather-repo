da51ad20ce6863af2a1561a4644c5ea54a8d6b5a
Lewis Benson <tm.lewisbenson@gmail.com>
2025-05-02 18:33:34 -0400

    update config

--- webpack.config.js
+++ webpack.config.js
@@ -54,6 +54,9 @@ export default (_env, argv) => {
     },
     plugins: [
       new DotenvWebpackPlugin(),
+      new webpack.DefinePlugin({
+        "process.env.WEATHER_KEY": JSON.stringify(process.env.WEATHER_KEY),
+      }),
       new HtmlWebpackPlugin({ template: "public/index.html" }),
       !prod && new ReactRefreshWebpackPlugin(),
     ].filter(Boolean),
